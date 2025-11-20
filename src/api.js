413c6483438ceda1774548a25b21992de1152c83
abdulhadi lbabidi <141328373+abdulhadi-lbabidi@users.noreply.github.com>
2025-11-20 14:05:55 +0300

    test

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://finince-api-test.nouh-agency.com/api",
   // You can add headers here if needed, e.g. Authorization
 });
 
